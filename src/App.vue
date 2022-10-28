8076934df9b5618680d7a00de6a7abc4cb0f4003
MAXBIT\v.zybailo <v.zybailo@jetmail.cc>
2022-10-28 16:14:25 +0300

    start

---
+++ src/App.vue
@@ -0,0 +1,47 @@
+<template>
+  <div id="app">
+    <h2>{{title}}</h2>
+    <ListTask 
+      :msg="msg"
+      @removeTitle="removeTitle"/> 
+  </div>
+</template>
+
+<script>
+import ListTask from '@/components/ListTask.vue'
+
+export default {
+  components: {
+    ListTask
+  },
+  data() {
+    return {
+      title: 'ToDO List',
+      todolist: [
+
+      ]
+    }
+  },
+   methods: {
+    changeTitle() {
+      this.title = this.newTitle
+      this.newTitle = ''
+    },
+     removeTitle(){
+      this.msg = ''
+      console.log('work')
+    }
+  },
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/ListTask.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="hello">
+
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    msg: String
+  },
+  methods: {
+    removeTitle() {
+      this.$emit('removeTitle', this.msg) 
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
